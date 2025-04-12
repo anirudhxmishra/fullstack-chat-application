e848b953c80f87057fae271b0f20d0e77ed5b18c
Anirudh Mishra <anirudhmishra112233@gmail.com>
2025-04-12 18:03:31 +0530

    fixed some error

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,16 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {getUsersForSidebar, sendMessage} from "../controllers/message.controller.js"
-import { getMessages } from "../controllers/message.controller.js";
-
+import { getUsersForSidebar, sendMessage, getMessages } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-
-router.get("/users",protectRoute,getUsersForSidebar);
-router.get("/:id",protectRoute,getMessages);
-
-router.post("/send/:id",protectRoute, sendMessage)
-
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/chat/:id", protectRoute, getMessages);  // <-- changed this
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
